af35141e73b4d8930f7c56394f3dd1bebe15d1a0
yugo-ibuki <y.ibuki91@gmail.com>
2022-02-25 20:30:44 +0900

    fix: 2

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 (() => {
     // リセット
-<<<<<<< Updated upstream
-    console.log('jjj')
-    console.log('kkk')
-    console.log('lll')
-    console.log(';;;')
-    console.log(' ')
-    console.log('111')
-    console.log('111')
-    console.log('222')
-    console.log('333')
-    console.log('666')
-    console.log('777')
-    console.log('888')
-    console.log('999')
-=======
     console.log('1')
     console.log('2')
->>>>>>> Stashed changes
 })()
