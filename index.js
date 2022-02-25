67c27d390c5dab59c1d49bd7e1deb6fe428db182
yugo-ibuki <67675670+yugo-ibuki@users.noreply.github.com>
2022-02-25 20:32:08 +0900

    fix: 1 (#66)

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 (() => {
     // リセット
-    console.log('jjj')
-    console.log('kkk')
-    console.log('lll')
-    console.log(';;;')
-    console.log(' ')
-    console.log('111')
-    console.log('111')
-    console.log('222')
-    console.log('333')
-    console.log('666')
-    console.log('777')
-    console.log('888')
-    console.log('999')
 })()
